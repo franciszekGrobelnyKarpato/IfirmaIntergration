0276d6026ca1354a8806fc3c6bad3ca0fe1188cf
franekKarpato <franciszek.grobelny@Karpato.pl>
2021-03-03 13:05:49 +0100

    fix 03.03

--- retrive/aura/OpportunityIfirmaService/OpportunityIfirmaServiceController.js
+++ retrive/aura/OpportunityIfirmaService/OpportunityIfirmaServiceController.js
@@ -1,50 +1,29 @@
 ({	
     init : function(component, event, helper) { 
-       
-        let action = component.get("c.createInvoiceByOpportunity");        
+        let action = component.get("c.checkFilledFields");        
         var recordId = component.get("v.recordId");
     	action.setParams({recordId:recordId});
-        
-        action.setCallback(this, function(response){
-        	let state = response.getState();                 
-            let responseCode = JSON.stringify(response.getReturnValue().responseCode);
-                        
-            if (state === "SUCCESS") {
-                var toastEvent = $A.get("e.force:showToast");
-                if(responseCode === "0"){
-                    toastEvent.setParams({                
-                        type: 'Success',                                        
-                        title: JSON.stringify(response.getReturnValue().info) ,                          
-                        message:'Login into Ifirma.pl service to edit or delete invoice.'                   
-                    }); 
-                }else{
-                    toastEvent.setParams({                
-                        type: 'Error',                                        
-                        message: JSON.stringify(response.getReturnValue().info) ,  
-                        mode: 'Sticky',                                                                
-                    }); 
-                }                
-                toastEvent.fire(); 
-            }else {              
-                
-                let errors = response.getError();
-                let message = "Uknown error";
-                
-                if (errors && Array.isArray(errors) && errors.length > 0) {
-                    message = errors[0].message;                    
-                }                                                   
-                var toastEvent = $A.get("e.force:showToast");
-                toastEvent.setParams({
-                    type: 'Error',
-                    message: "State: " + state + ' - ' + message,                     
-                });
-                toastEvent.fire();
-            }
-                       
-    		component.set("v.loaded", false);   
-            $A.get("e.force:closeQuickAction").fire();
-        });
 
-		$A.enqueueAction(action);   
+        action.setCallback(this, function(response){
+        	let state = response.getState();    
+            if(state === "SUCCESS"){
+                let toastEvent = $A.get("e.force:showToast");
+                let answer = response.getReturnValue() 
+                if(answer.length != 0){                    
+                    toastEvent.setParams({                
+                        type: 'Error',                                                                                                                  
+                        message:'Fill fields '+answer ,
+                        mode: 'Sticky'                
+                    });
+                    toastEvent.fire(); 
+                    component.set("v.loaded", false); 
+                    $A.get("e.force:closeQuickAction").fire();
+                }   
+                else{                                       
+                    helper.createInvoice(component);
+                }                     
+            }               
+        });
+        $A.enqueueAction(action); 
 	}
 })
